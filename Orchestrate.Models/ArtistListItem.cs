19b31d159571168151218823045f1f8725d9710a
unknown <lukedtitus@gmail.com>
2018-11-28 11:37:47 -0500

    artist view created

---
+++ Orchestrate.Models/ArtistListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchestrate.Models
+{
+    public class ArtistListItem
+    {
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Orchestrate.WebMVC/Controllers/ArtistController.cs
@@ -0,0 +1,20 @@
+﻿using Orchestrate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Orchestrate.WebMVC.Controllers
+{
+    [Authorize]
+    public class ArtistController : Controller
+    {
+        // GET: Artist
+        public ActionResult Index()
+        {
+            var model = new ArtistListItem[0];
+            return View(model);
+        }
+    }
+}
