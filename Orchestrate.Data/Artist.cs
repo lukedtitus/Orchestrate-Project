cf195feecf0d742877069dd6bb0115334c2463a7
unknown <lukedtitus@gmail.com>
2018-11-28 10:57:31 -0500

    database set up

---
+++ Orchestrate.Data/Artist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchestrate.Data
+{
+    public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string ArtistName { get; set; }
+
+        [Required]
+        public int NumberOfMembers { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public int ProjectsReleased { get; set; }
+    }
+}
